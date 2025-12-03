f8cc7cf81974a3b31a1d2c1d6f48b0f910c7e3ac
SharlottaL <botanichkagigantika@gmail.com>
2025-12-03 12:19:53 +0300

    In Progress 2

--- TextRedac/MainWindow.xaml.cs
+++ TextRedac/MainWindow.xaml.cs
@@ -39,18 +39,52 @@ namespace TextRedac
             dialog.Filter = "Текстовые документы (*.txt)|*.txt|Файлы Markdown (*.md)|*.md|All files (*.*)|*.*";
             dialog.FilterIndex = 1;
             //dialog.ShowDialog();
+          
             if (dialog.ShowDialog() == true && !string.IsNullOrWhiteSpace(dialog.FileName))
             {
-                string fileSod = File.ReadAllText(dialog.FileName);
+                StreamReader read = new StreamReader(dialog.FileName);
+                string fileSod = read.ReadLine();
                 textObl.Text = fileSod;
                 MessageBox.Show(dialog.FileName, "Filename", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
-        
+
         private void MenuItem_Click_3(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+        private string currentFilePath;
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "Текстовые документы (*.txt)|*.txt|Файлы Markdown (*.md)|*.md|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.DefaultExt = ".txt";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                StreamWriter write = new StreamWriter(saveFileDialog.FileName);
+              
+                    write.Write(textObl.Text);
+                    write.Close();
+                    //File.WriteAllText(saveFileDialog.FileName, textObl.Text);
+                    MessageBox.Show(saveFileDialog.FileName, "Filename", MessageBoxButton.OK, MessageBoxImage.Information);
+                    currentFilePath = filePath;
+            }
+        }
+      
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (currentFilePath == null)
+                MenuItem_Click_1(sender, e);
+            else
+            {
+                File.WriteAllText(currentFilePath, textObl.Text);
+                MessageBox.Show("Файл перезаписан!");
+            }
+        }
     }
 }
+
 
